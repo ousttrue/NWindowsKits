285f3c896143ca1d4ebab824a167945cc26af940
ousttrue <ousttrue@gmail.com>
2020-06-24 20:24:26 +0900

    Dispose

--- NWindowsKits/Samples/DXGIEnumerateAdapters/Program.cs
+++ NWindowsKits/Samples/DXGIEnumerateAdapters/Program.cs
@@ -7,25 +7,29 @@ namespace DXGIEnumerateAdapters
     {
         static void Main(string[] args)
         {
-            var factory = new IDXGIFactory();
-            var hr = dxgi.CreateDXGIFactory(ref factory.GetIID(), ref factory.PtrForNew);
-            if (hr != 0)
+            using (var factory = new IDXGIFactory())
             {
-                throw new ComException(hr);
-            }
-
-            for (uint i = 0; ; ++i)
-            {
-                var adapter = new IDXGIAdapter();
-                hr = factory.EnumAdapters(i, ref adapter.PtrForNew);
+                var hr = dxgi.CreateDXGIFactory(ref factory.GetIID(), ref factory.PtrForNew);
                 if (hr != 0)
                 {
-                    break;
+                    throw new ComException(hr);
                 }
 
-                DXGI_ADAPTER_DESC desc = default;
-                adapter.GetDesc(ref desc);
-                Console.WriteLine(desc.Description);
+                for (uint i = 0; ; ++i)
+                {
+                    using (var adapter = new IDXGIAdapter())
+                    {
+                        hr = factory.EnumAdapters(i, ref adapter.PtrForNew);
+                        if (hr != 0)
+                        {
+                            break;
+                        }
+
+                        DXGI_ADAPTER_DESC desc = default;
+                        adapter.GetDesc(ref desc);
+                        Console.WriteLine(desc.Description);
+                    }
+                }
             }
         }
     }
