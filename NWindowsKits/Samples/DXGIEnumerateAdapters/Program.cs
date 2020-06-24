c78ca9432af0975300540011feb1f2b45572c900
ousttrue <ousttrue@gmail.com>
2020-06-24 19:03:33 +0900

    WIP DXGIEnumerateAdapters

---
+++ NWindowsKits/Samples/DXGIEnumerateAdapters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NWindowsKits;
+
+namespace DXGIEnumerateAdapters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new IDXGIFactory();
+            var hr = dxgi.CreateDXGIFactory(ref factory.GetIID(), ref factory.PtrForNew);
+            if (hr != 0)
+            {
+                throw new ComException(hr);
+            }
+
+            for (uint i = 0; ; ++i)
+            {
+                var adapter = new IDXGIAdapter();
+                hr = factory.EnumAdapters(i, ref adapter.PtrForNew);
+                if (hr != 0)
+                {
+                    break;
+                }
+
+                DXGI_ADAPTER_DESC desc = default;
+                adapter.GetDesc(ref desc);
+                Console.WriteLine(desc.Description);
+            }
+        }
+    }
+}
